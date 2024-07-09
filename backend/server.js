d455bae8b8224115120cdfa861bd65a823497102
darkars33 <darshangarad18@gmail.com>
2024-07-09 18:08:09 +0530

    change the limit of image size

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
           origin: '*',
           credentials: true,
 }))
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 

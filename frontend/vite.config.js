3b0bec7566384b1e8f5d849cb0a89a8d82b86bf5
darkars33 <darshangarad18@gmail.com>
2024-07-11 18:32:08 +0530

    minor fix 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy:{
       "/api":{
-        target: "",
+        target: "https://x-mi5s.onrender.com",
         changeOrigin: true,
         secure: false,
       }
